112b958425b9fd686ecbc032b3c8d0b713b8aa2d
riserishitha <rishitha.n@kalvium.community>
2023-11-29 11:32:44 +0530

    commit changes

--- src/app.js
+++ src/app.js
@@ -24,9 +24,16 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = '';
-
     // Instead of forloop use Map method
     // Code here
+    detailedPlayers = players.map((Element, index) =>{
+        return{
+            name: Element,
+            strength: getRandomStrength(),
+            img:`images/super-${index+1}.png`,
+            type: index % 2 == 0 ? 'hero' : 'villain',
+        };
+    });
 
     return detailedPlayers;
 }
@@ -43,7 +50,17 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    fragment = players.filter((element, index) => {
+      return element.type == type;
+    });
+    fragment = fragment.map((element, index) => {
+        return `<div class="player">
+        <img src="${element.img}">
+         <div class="name">${element.name}</div>
+         <div class="strength">${element.strength}</div>
+     </div>`
+    });
+ 
     return fragment;
 }
 
